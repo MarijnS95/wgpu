d039f58525cdf236bdf12701020871bd20d20189
Marijn Suijten <marijns95@gmail.com>
2024-08-24 10:19:35 +0200

    Playing with `DirectComposition`
    https://learn.microsoft.com/en-us/archive/msdn-magazine/2014/june/windows-with-c-high-performance-window-layering-using-the-windows-composition-engine

--- wgpu-hal/src/dx12/mod.rs
+++ wgpu-hal/src/dx12/mod.rs
@@ -1287,6 +1287,7 @@ impl crate::Surface for Surface {
                         profiling::scope!(
                             "IDXGIFactoryMedia::CreateSwapChainForCompositionSurfaceHandle"
                         );
+                        dbg!("IDXGIFactoryMedia::CreateSwapChainForCompositionSurfaceHandle");
                         unsafe {
                             self.factory_media
                                 .as_ref()
